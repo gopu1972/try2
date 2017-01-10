1c063d4537a3c14b7529daa3674f35b0ee0ff91f
Gopu <gopu.shrestha2@gmail.com>
2017-01-10 14:12:09 -0600

    added method

---
+++ MyGitJava/src/com/sample/TestGit.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		callHello();
+		
+		
+		
+	}
+
+	private static void callHello() {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+	}
+}
